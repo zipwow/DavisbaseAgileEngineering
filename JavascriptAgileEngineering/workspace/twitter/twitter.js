7820b79d073dc597006945990bdb457f190825e9
Kevin Klinemeier <zipwow@gmail.com>
2013-04-22 23:46:54 -0700

    exercise cleanup

--- JavascriptAgileEngineering/workspace/twitter/twitter.js
+++ JavascriptAgileEngineering/workspace/twitter/twitter.js
@@ -4,12 +4,21 @@ $.ajax({
         url: 'http://search.twitter.com/search.json?q=' + jQuery.param(query),
         dataType: 'jsonp',
         success: function(data) {
-            var tweets = $(selector);
-            tweets.html('');
-            for (res in data['results']) {
-                tweets.append('<div>' + data['results'][res]['from_user'] + 
-                		' wrote: <p>' + data['results'][res]['text'] + '</p></div><br />\n');
-        	}
+                delayedAddTweets(selector, data);
         }
     });
 }
+
+function delayedAddTweets(selector, data) {
+    var tweets = $(selector);
+    tweets.html('');
+    for (result in data['results']) {
+        //imagine fade-ins, or other more interesting animation here that consume time to execute and execute 
+        //asynchronously.
+        setTimeout(function (){
+            tweets.append('<div>' + data['results'][result]['from_user'] + 
+                    ' wrote: <p>' + data['results'][result]['text'] + '</p></div><br />\n');
+       },50);
+    }           
+}
+ 
