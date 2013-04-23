ef0e024ead5e52b35111976e45ddec3c325f60ab
Kevin Klinemeier <zipwow@gmail.com>
2013-04-22 22:11:24 -0700

    cleaned up dual workspaces

--- JavascriptAgileEngineering/instructor workspace/queue/queue.js
+++
@@ -1,3 +0,0 @@
-//if I were queue code, this is where I would be. 
-//but I'm not here, since we're doing TDD
-
--- JavascriptAgileEngineering/student workspace/queue/queueTests.js
+++
@@ -1,4 +0,0 @@
-module("queue");
-test("hello world", function () {
-	ok(true,true,"the world is stable, hello world");
-})
