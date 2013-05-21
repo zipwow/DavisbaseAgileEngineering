bd9f0f99583c9a385cffd5f8bde068099b9be7d2
Kevin Klinemeier <zipwow@gmail.com>
2013-05-21 14:47:13 -0700

    removed code from PetStore, see hidden file for solution

--- eclipseWorkspace/agileEngineeringCucumber/src/main/java/com/davisbase/genepets/PetStore.java
+++ eclipseWorkspace/agileEngineeringCucumber/src/main/java/com/davisbase/genepets/PetStore.java
@@ -1,17 +1,6 @@
 package com.davisbase.genepets;
 
-import java.math.BigDecimal;
 
 public class PetStore {
-
-	public void addPet(String petName, BigDecimal price) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public BigDecimal search(String petName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
